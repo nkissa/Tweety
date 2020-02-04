69428d75700e560d249c447a4b44894e9e9138cb
Nasar Issa <nkissa@ucsd.edu>
2020-02-03 16:53:22 -0800

    Added Stay Logged In Functionality

--- Twitter/LoginViewController.swift
+++ Twitter/LoginViewController.swift
@@ -16,11 +16,21 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        if(UserDefaults.standard.bool(forKey: "userLoggedIn")) == true{
+            self.performSegue(withIdentifier: "LoginToHome", sender: self)
+        }
+    }
+    
     @IBAction func onLoginButton(_ sender: Any) {
         
         let myUrl = "https://api.twitter.com/oauth/request_token"
         TwitterAPICaller.client?.login(url: myUrl, success: {
-            self.performSegue(withIdentifier: "LoginToHome", sender: self)
+            
+            UserDefaults.standard.set(true, forKey: "userLoggedIn")
+            
+            self.performSegue(withIdentifier: "LoginToHome", sender: self
+            )
         }, failure: { (Error) in
             print("Could not login.")
         })
