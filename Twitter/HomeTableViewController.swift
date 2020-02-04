8f89c875cd9d6b95a4c54b05153068101b169cf2
Nasar Issa <nkissa@ucsd.edu>
2020-02-04 14:20:41 -0800

    Debugging and Minor changes

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -35,7 +35,7 @@ class HomeTableViewController: UITableViewController {
         let myURL = "https://api.twitter.com/1.1/statuses/home_timeline.json"
         let myParams = ["count":numberOfTweets]
         
-        TwitterAPICaller.client?.getDictionariesRequest(url: myURL, parameters: myParams, success: { (tweets: [NSDictionary]) in
+        TwitterAPICaller.client?.getDictionariesRequest(url: myURL, parameters: myParams as [String : Any], success: { (tweets: [NSDictionary]) in
             self.tweetArray.removeAll()
             for  tweet in tweets{
                 self.tweetArray.append(tweet)
@@ -53,7 +53,7 @@ class HomeTableViewController: UITableViewController {
         numberOfTweets += 20
         let myParams = ["count":numberOfTweets]
         
-        TwitterAPICaller.client?.getDictionariesRequest(url: myURL, parameters: myParams, success: { (tweets: [NSDictionary]) in
+        TwitterAPICaller.client?.getDictionariesRequest(url: myURL, parameters: myParams as [String : Any], success: { (tweets: [NSDictionary]) in
             self.tweetArray.removeAll()
             for  tweet in tweets{
                 self.tweetArray.append(tweet)
